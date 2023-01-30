5c78ce70b838e3b6380a9c48549d8b5aba25d1e1
mdesabd <esa.ce2020@gmail.com>
2023-01-30 22:53:28 +0600

    signUp intregation done

--- src/Pages/SingUp/SignUp.js
+++ src/Pages/SingUp/SignUp.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 
 const SignUp = () => {
-   
-    const handleSignUp = event =>{
-       
+    const { createUser } = useContext(AuthContext);
+    const handleSignUp = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(err => console.error(err));
     }
+
 
     return (
         <div className="hero w-full my-0">
@@ -29,14 +41,14 @@ const SignUp = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" name='email' placeholder="email" className="input input-bordered" required/>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" name='password' placeholder="password" className="input input-bordered" required/>
-                            
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" required />
+
                         </div>
                         <div className="form-control mt-6">
                             <input className="btn btn-primary" type="submit" value="Sign Up" />
