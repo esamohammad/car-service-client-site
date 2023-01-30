1078f664ba5dbb665fe39342af2d1829e73dd2b1
mdesabd <esa.ce2020@gmail.com>
2023-01-30 15:31:06 +0600

    services data loaded locally

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import About from '../About/About';
+import Services from '../Services/Services';
 
 const Home = () => {
    return (
       <div>
          <Banner></Banner>
          <About></About>
+         <Services></Services>
       </div>
    );
 };
