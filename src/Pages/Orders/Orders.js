54e4c256fdbd19a5da6b4733a3732c2916be932a
mdesabd <esa.ce2020@gmail.com>
2023-02-01 07:26:17 +0600

    display orders in a table

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import OrderRow from './OrderRow';
 
 const Orders = () => {
    const { user } = useContext(AuthContext);
@@ -14,8 +15,33 @@ const Orders = () => {
 
    return (
       <div>
-         <h2 className="text-5xl">You have {orders.length} Orders</h2>
-      </div>
+            <h2 className="text-5xl">You have {orders.length} Orders</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <OrderRow
+                                key={order._id}
+                                order={order}
+                            ></OrderRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
    );
 };
 
