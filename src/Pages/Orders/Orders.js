9a76d0fd014e704a3730820b7d704ae078a09085
mdesabd <esa.ce2020@gmail.com>
2023-02-01 11:41:09 +0600

    update implement

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -8,59 +8,83 @@ const Orders = () => {
 
    useEffect(() => {
 
-      fetch(`http://localhost:5000/orders?email=${user.email}`)
+      fetch(`http://localhost:5000/orders?email=${user?.email}`)
          .then(res => res.json())
          .then(data => setOrders(data))
    }, [user?.email])
 
-   const handleDelete = id =>{
+   const handleDelete = id => {
       const proceed = window.confirm('Are you sure, you want to cancel this order');
-      if(proceed){
-          fetch(`http://localhost:5000/orders/${id}`, {
-              method: 'DELETE'
-          })
-          .then(res => res.json())
-          .then(data => {
-              console.log(data);
-              if (data.deletedCount > 0){
+      if (proceed) {
+         fetch(`http://localhost:5000/orders/${id}`, {
+            method: 'DELETE'
+         })
+            .then(res => res.json())
+            .then(data => {
+               console.log(data);
+               if (data.deletedCount > 0) {
                   alert('deleted successfully');
                   const remaining = orders.filter(odr => odr._id !== id);
                   setOrders(remaining);
-              }
-          })
+               }
+            })
       }
-  }
+   }
+
+
+   const handleStatusUpdate = id => {
+      fetch(`http://localhost:5000/orders/${id}`, {
+         method: 'PATCH',
+         headers: {
+            'content-type': 'application/json'
+         },
+         body: JSON.stringify({ status: 'Approved' })
+      })
+         .then(res => res.json())
+         .then(data => {
+            console.log(data);
+            if (data.modifiedCount > 0) {
+               const remaining = orders.filter(odr => odr._id !== id);
+               const approving = orders.find(odr => odr._id === id);
+               approving.status = 'Approved'
+
+               const newOrders = [approving, ...remaining];
+               setOrders(newOrders);
+            }
+         })
+   }
 
    return (
       <div>
-            <h2 className="text-5xl">You have {orders.length} Orders</h2>
-            <div className="overflow-x-auto w-full">
-                <table className="table w-full">
-                    <thead>
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            orders.map(order => <OrderRow
-                                key={order._id}
-                                order={order}
-                                handleDelete={handleDelete}
-                            ></OrderRow>)
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </div>
+         <h2 className="text-5xl">You have {orders.length} Orders</h2>
+         <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+               <thead>
+                  <tr>
+                     <th>
+                        <label>
+                           <input type="checkbox" className="checkbox" />
+                        </label>
+                     </th>
+                     <th>Name</th>
+                     <th>Job</th>
+                     <th>Favorite Color</th>
+                     <th></th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {
+                     orders.map(order => <OrderRow
+                        key={order._id}
+                        order={order}
+                        handleDelete={handleDelete}
+                        handleStatusUpdate={handleStatusUpdate}
+                     ></OrderRow>)
+                  }
+               </tbody>
+            </table>
+         </div>
+      </div>
    );
 };
 
