2e23abd9985d0e99174bff88c3feeec3aed48df0
mdesabd <esa.ce2020@gmail.com>
2023-02-02 23:12:38 +0600

    jwt token 1/3 done show fist the email

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -20,10 +20,36 @@ const Login = () => {
         login(email, password)
             .then(result => {
                 const user = result.user;
-                console.log(user);
-                navigate(from, { replace: true });
+
+
+
+
+                const currentUser = {
+                    email: user.email
+                }
+
+                console.log(currentUser);
+
+                //get jwt token 
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                    })
+
+
+
+
+
+                // navigate(from, { replace: true });
             })
-            .then(error => console.log(error));
+            .catch(error => console.log(error));
     }
 
     return (
